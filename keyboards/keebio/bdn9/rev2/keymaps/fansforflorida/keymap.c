92c18b8dec12a95ea3fd9e66302d888337cce758
fansforflorida <jhall@alum.wpi.edu>
2025-02-14 21:41:19 -0500

    Rewrote layer indicator lighting code

--- keyboards/keebio/bdn9/rev2/keymaps/fansforflorida/keymap.c
+++ keyboards/keebio/bdn9/rev2/keymaps/fansforflorida/keymap.c
@@ -15,89 +15,76 @@
 
 #include QMK_KEYBOARD_H
 
-enum layer_names {
-    _VSTUDIO, // Visual Studio
-    _ECLIPSE, // Eclipse
-    _EMPTY2,
-    _EMPTY3
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-    /* 
-     * .--------------------.---------------.-------------.
-     * | Open Type          | Change Layers | Open File   |
-     * :--------------------+---------------+-------------:
-     * | Toggle Breakpoints | Run/Resume    | End         |
-     * :--------------------+---------------+-------------:
-     * | Step Over          | Step Into     | Step Return |
-     * '--------------------'---------------'-------------'
-     */
-
-    [_VSTUDIO] = LAYOUT(
-        C(KC_T),     TO(1),  C(KC_O),
-        C(S(KC_F9)), KC_F5,  S(KC_F5),
-        KC_F10,      KC_F11, S(KC_F11)
+    [0] = LAYOUT(
+        KC_0,    TO(1),   KC_0,
+        KC_0,    KC_0,    KC_0,
+        KC_0,    KC_0,    KC_0
     ),
-
-    [_ECLIPSE] = LAYOUT(
-        C(S(KC_T)),  TO(0),  C(S(KC_R)),
-        LCA(KC_B),   KC_F8,  C(KC_F2),
-        KC_F6,       KC_F5,  KC_F7
+    [1] = LAYOUT(
+        KC_1,    TO(2),   KC_1,
+        KC_1,    KC_1,    KC_1,
+        KC_1,    KC_1,    KC_1
     ),
-
-    [_EMPTY2] = LAYOUT(
-        XXXXXXX, TO(3),   XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX
+    [2] = LAYOUT(
+        KC_2,    TO(3),   KC_2,
+        KC_2,    KC_2,    KC_2,
+        KC_2,    KC_2,    KC_2
     ),
-
-    [_EMPTY3] = LAYOUT(
-        XXXXXXX, TO(0),   XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX
-    )
-
+    [3] = LAYOUT(
+        KC_3,    TO(4),   KC_3,
+        KC_3,    KC_3,    KC_3,
+        KC_3,    KC_3,    KC_3
+    ),
+    [4] = LAYOUT(
+        KC_4,    TO(5),   KC_4,
+        KC_4,    KC_4,    KC_4,
+        KC_4,    KC_4,    KC_4
+    ),
+    [5] = LAYOUT(
+        KC_5,    TO(0),   KC_5,
+        KC_5,    KC_5,    KC_5,
+        KC_5,    KC_5,    KC_5
+    ),
 };
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [_VSTUDIO] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
-    [_ECLIPSE] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
-    [_EMPTY2] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
-    [_EMPTY3] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
+    [0] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(RGB_VAD, RGB_VAI), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
+    [1] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
+    [2] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
+    [3] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
+    [4] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
+    [5] = { ENCODER_CCW_CW(XXXXXXX, XXXXXXX), ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(XXXXXXX, XXXXXXX) },
 };
 #endif
 
 #ifdef RGB_MATRIX_ENABLE
+uint8_t rgb_to_hue(uint8_t rgb_mask) {
+    uint8_t r = (rgb_mask & 0b100) >> 2;
+    uint8_t g = (rgb_mask & 0b010) >> 1;
+    uint8_t b = rgb_mask & 0b001;
+    uint8_t c_max = r | g | b;
+    int h;
+    if (c_max == r) {
+        h = 60 * ((g - b + 6) % 6);
+    } else if (c_max == g) {
+        h = 60 * (b - r + 2);
+    } else if (c_max == b) {
+        h = 60 * ((r - g) + 4);
+    }
+    return h * 255 / 360;
+}
+
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-    HSV hsv;
-
-    switch(get_highest_layer(layer_state | default_layer_state)) {
-        case _VSTUDIO:
-            hsv = (HSV){HSV_PURPLE};
-            break;
-        case _ECLIPSE:
-            hsv = (HSV){HSV_RED};
-            break;
-        case _EMPTY2:
-            hsv = (HSV){HSV_GREEN};
-            break;
-        case _EMPTY3:
-        default:
-            hsv = (HSV){HSV_BLUE};
-            break;
-    }
-
-    hsv.v = rgb_matrix_get_val();
-
-    RGB rgb = hsv_to_rgb(hsv);
-    for (uint8_t i = led_min; i < led_max; i++) {
-        // To only affect RGB underglow, add this:
-        // if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_UNDERGLOW)) {
-        rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-    }
-
+    uint8_t layer = get_highest_layer(layer_state | default_layer_state);
+    hsv_t hsv = {
+        .h = rgb_to_hue(layer + 1),
+        .s = 255,
+        .v = rgb_matrix_get_val()
+    };
+    rgb_t rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color_all(rgb.r, rgb.g, rgb.b);
     return false;
 }
 #endif
