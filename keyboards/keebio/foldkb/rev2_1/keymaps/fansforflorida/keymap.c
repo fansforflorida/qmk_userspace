d5e94a4f6b2e362737a8bc3e0672d3535c3aeba4
John Hall <jhall@alum.wpi.edu>
2025-12-23 20:55:48 -0500

    Renamed rev2 to rev2_1

--- keyboards/keebio/foldkb/rev2_1/keymaps/fansforflorida/keymap.c
+++ keyboards/keebio/foldkb/rev2_1/keymaps/fansforflorida/keymap.c
@@ -13,41 +13,14 @@
 #define SPC_FN3 LT(3,KC_SPC)
 
 enum custom_keycodes {
-    CD_HOME = SAFE_RANGE,
-    RM_TEST, // TODO This is just for testing
+    CD_HOME = SAFE_RANGE
 };
-
-// TODO This is just for testing
-const uint8_t rgb_matrix_split[2] = RGB_MATRIX_SPLIT;
-uint8_t led_index = rgb_matrix_split[0];
-
-#ifdef RGB_MATRIX_ENABLE
-// TODO This is just for testing
-// bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
-//     for (uint8_t i = led_min; i < led_max; i++) {
-//         if (i != led_index) {
-//             rgb_matrix_set_color(i, RGB_OFF);
-//         }
-//     }
-//     return false;
-// }
-#endif
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case CD_HOME:
             if (record->event.pressed) {
                 SEND_STRING("cd ~\n");
-            }
-            return false;
-
-        // TODO This is just for testing
-        case RM_TEST:
-            if (record->event.pressed) {
-                led_index++;
-                if (led_index == RGB_MATRIX_LED_COUNT) {
-                    led_index = rgb_matrix_split[0];
-                }
             }
             return false;
 
@@ -121,9 +94,9 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC] = LAYOUT(
-        RM_TOGG, KC_ESC,  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        EE_CLR,  KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSLS, KC_DEL,
+        RM_TOGG, KC_ESC,  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_BSLS, KC_DEL,
         KC_HOME,          KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_END,           KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          RM_TEST,
+        KC_END,           KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_PGUP,          KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    TD_COMM, KC_DOT,  KC_SLSH, KC_UP,   MO(2),
         KC_PGDN,          KC_LCTL, MO(2),   KC_LGUI, KC_LALT, SPC_FN3, SPC_FN3,     KC_SPC,  KC_SPC,           KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
     ),
